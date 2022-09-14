fed9d3488a02e3076acaf1bbff279163de025228
Chapin <23ChapinK@mydacc.org>
2022-09-14 14:16:23 -0400

    fr

--- javascript/filter.js
+++ javascript/filter.js
@@ -1,25 +1,30 @@
-function hondaFunction() {
-    var x = document.getElementById("HondaDiv");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+function addMyItem(make){
+  var myHTML = "<a href=\"onclick=addItem2(1)\">Ranger</a>";
+  myHTML += "<a href=\"onclick=addItem2(1)\">F150</a>";
+  myHTML += "<a href=\"onclick=addItem2(1)\">Mustang</a>";
+  switch(make) {
+      case 1:
+          myHTML = "<a href=\"onclick=addItem2(1)\">Blazer</a>";
+          myHTML += "<a href=\"onclick=addItem2(1)\">Silverado</a>";
+          myHTML += "<a href=\"onclick=addItem2(1)\">Colorado</a>";
+          break;
+      case 2:
+          myHTML = "<a href=\"onclick=addItem2(1)\">Ram</a>";
+          myHTML += "<a href=\"onclick=addItem2(1)\">Challenger</a>";
+          myHTML += "<a href=\"onclick=addItem2(1)\">Charger</a>";
+          break;
+      case 3:
+          myHTML = "<a href=\"onclick=addItem2(1)\">Ranger</a>";
+          myHTML += "<a href=\"onclick=addItem2(1)\">F150</a>";
+          myHTML += "<a href=\"onclick=addItem2(1)\">Mustang</a>";
+          break;
+      case 4:
+          myHTML = "<a href=\"onclick=addItem2(1)\">Cyclone</a>";
+          myHTML += "<a href=\"onclick=addItem2(1)\">Jimmy</a>";
+          myHTML += "<a href=\"onclick=addItem2(1)\">Sierra</a>";    
+          break;
+      default:
+          console.log('Error Occured!!!');
   }
-  function fordFunction() {
-    var x = document.getElementById("FordDiv");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-  function dodgeFunction() {
-    var x = document.getElementById("dodgeDiv");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-  
+  document.getElementById("modelMenu").innerHTML = myHTML;
+}

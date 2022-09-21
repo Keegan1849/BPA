db52b7387b4da79877d6e2b6ad6c7c03c93a2e19
Chapin <23ChapinK@mydacc.org>
2022-09-21 14:16:39 -0400

    fr

--- javascript/nav.js
+++ javascript/nav.js
@@ -23,3 +23,15 @@
                         .style.fontSize = "20px";
         }
     }
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("socials").style.top = "0";
+  } else {
+    document.getElementById("socials").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
